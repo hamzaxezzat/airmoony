aa8e38e0fce255bb1ee4e8948cae18d709349baa
hamza ezzat <hamza2007ezzat@gmail.com>
2023-03-01 20:55:17 +0100

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 
-import './App.css';
+import {Switch, Route} from "react-router-dom"
 
-const App= ()=>(
-  <div>
-    <p>React App</p>
-  </div>
-) 
+import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+
+const App= ()=>{
+  return(
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/register" component={Register}/>
+    </Switch>
+  )
+}
 
 export default App;
